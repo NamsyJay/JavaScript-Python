ca0fed4a73e1ad37658301a8a199abdc789afd95
namsyjay <onamusibabajide@gmail.com>
2024-11-21 19:52:38 +0000

    Changes to Flask Application

--- Full Stack Application/Frontend/src/App.jsx
+++ Full Stack Application/Frontend/src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-
-
-import './App.css'
+import { useState, useEffect } from "react";
+import ContactList from "./ClientLists";
+import "./App.css";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetchContacts();
+  }, []);
+
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
+    console.log(data.contacts);
+  };
 
   return (
     <>
-      
+      <ContactList contacts={contacts} />
+      <ContactForm />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
